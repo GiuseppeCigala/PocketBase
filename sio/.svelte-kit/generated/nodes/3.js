689a84aabcfcc2459b1cbc8d9107f4f75facd75d
Giuseppe Cigala <info@giuseppecigala.it>
2022-10-02 06:01:06 +0200

    update source code

--- sio/.svelte-kit/generated/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "../../../src/routes/about/+page.js";
-export { shared };
-export { default as component } from "../../../src/routes/about/+page.svelte";
--- sio/.svelte-kit/generated/nodes/4.js
+++
@@ -1,2 +0,0 @@
-export { default as component } from "../../../src/routes/todos/+page.svelte";
-export const server = true;
